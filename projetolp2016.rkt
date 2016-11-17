d433584c846cf3359178f0c83a8dab96e10c4aa1
Paula Bruna Daco <paulabrunadaco@gmail.com>
2016-11-17 19:57:41 -0200

    Update projetolp2016.rkt

--- projetolp2016.rkt
+++ projetolp2016.rkt
@@ -1,6 +1,6 @@
 #lang racket
 ;projeto lp-2016 paula rafael e waldo
-
+**Função X calcula a soma de um interavalo dado através de ponteiros numa lista.**
 (define (X lst ptr1 ptr2)
   (define (auxX lst ptr1 ptr2 counter)
     (if (equal? ptr1 ptr2)
@@ -8,7 +8,7 @@
         (auxX lst ptr1 (- ptr2 1) (+ counter (list-ref lst (- ptr2 1))))))
   (auxX lst ptr1 ptr2 (list-ref lst (- ptr1 1))))
 
-
+**Função MaxSoFar gera a maior soma possível de uma sublista de tamanho qualquer na lista.**
 (define (MaxSoFar lst)
   (let ((p '()))
     (if (null? lst)
@@ -17,7 +17,20 @@
               #:when (> i 0))
           (for ((j (range i (+ (length lst) 1))))
             (begin (set! p (append p (list (X lst i j))))
-                   (print p)))))))
+                   (print p))))))
+
+1ª mudança no código 1:
+
+(define (MaxSoFar lst)
+  (let ((p '()))
+    (if (null? lst)
+        '()
+        (for/list ((i (+ (length lst) 1))
+              #:when (> i 0))
+          (for/list ((j (range i (+ (length lst) 1))))
+            (begin (set! p (X lst i j))
+                   (list p)))))))
+(Problema: Geramos uma lista de somas possíveis, mas ainda não somente a maior soma.)
                    
 ;;;Código original do artigo:
 ;MaxSoFar :-- 0.0
